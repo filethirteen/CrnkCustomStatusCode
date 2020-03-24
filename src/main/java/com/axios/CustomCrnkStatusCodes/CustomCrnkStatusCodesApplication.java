458b9c9992dd2a0d6089dd3012e23cff375081cb
Kyle Bishop <kyle.bishop.wwz8@statefarm.com>
2020-03-23 17:07:05 -0700

    Initial commit base initializr service

---
+++ src/main/java/com/axios/CustomCrnkStatusCodes/CustomCrnkStatusCodesApplication.java
@@ -0,0 +1,13 @@
+package com.axios.CustomCrnkStatusCodes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomCrnkStatusCodesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomCrnkStatusCodesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/axios/CustomCrnkStatusCodes/CustomCrnkStatusCodesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axios.CustomCrnkStatusCodes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomCrnkStatusCodesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
